b2fb0b35edc5a83f6a8c67a1608cd4839b70cee6
Łukasz Kaźmierczak <lukask84@o2.pl>
2025-02-06 23:20:48 +0100

    Separating dygraph ranges on samples/time, workflow updated ver 2
    Signed-off-by: Łukasz Kaźmierczak <lukask84@o2.pl>

--- examples/dygraph/rangesamples/src/dygraph.cpp
+++ examples/dygraph/rangesamples/src/dygraph.cpp
@@ -16,20 +16,20 @@ int main()
             {{"Device B status", "label x", "label y"},
              {500, 250},
              {100ms, 100, {{"datab.csv", "time,state", 1000}}}});
-        graphA->start();
+        // graphA->start();
         // graphB->start();
 
-        graphs::helpers::TimeMonitor tm;
-        std::string data = std::to_string(tm.getseconds()) + "," + "200";
-        graphA->add(data);
-        sleep(1);
-        data = std::to_string(tm.getseconds()) + "," + "100";
-        graphA->add(data);
-        sleep(1);
-        data = std::to_string(tm.getseconds()) + "," + "300";
-        graphA->add(data);
-        while (true)
-            usleep(100 * 1000);
+        // graphs::helpers::TimeMonitor tm;
+        // std::string data = std::to_string(tm.getseconds()) + "," + "200";
+        // graphA->add(data);
+        // sleep(1);
+        // data = std::to_string(tm.getseconds()) + "," + "100";
+        // graphA->add(data);
+        // sleep(1);
+        // data = std::to_string(tm.getseconds()) + "," + "300";
+        // graphA->add(data);
+        // while (true)
+        //     usleep(100 * 1000);
     }
     catch (std::exception& err)
     {
